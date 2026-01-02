ed0b18bcdbb8fa14592ccf63b5e6fa007549559b
Kengineering <2023555461@ogr.cu.edu.tr>
2026-01-02 11:40:07 +0300

    assets.jsx baseUrl plugin

--- src/game/assets.jsx
+++ src/game/assets.jsx
@@ -1,160 +1,162 @@
+const baseUrl = import.meta.env.BASE_URL;
+
 export const fbxElements = [
     {
         id: 0,
-        disappear: 0, source: '/Rock&Cacti.fbx',
-        texture: '/plane_DefaultMaterial_BaseMap.png',
+        disappear: 0, source: `${baseUrl}Rock&Cacti.fbx`,
+        texture: `${baseUrl}plane_DefaultMaterial_BaseMap.png`,
         rotation: [0, 0, 0],
         scale: [0.001, 0.002, 0.001],
         position: [0, 1, -100]
     },
     {   
         id: 1, 
-        disappear: 0, source: '/Hill_desert_001.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Hill_desert_001.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.002, 0.001],
         position: [0, 0, -100]
     },
     {
         id: 2, 
-        disappear: 0, source: '/Hill_desert_002.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Hill_desert_002.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.002, 0.001],
         position: [3, 0, -100]
     },
     {
         id: 3, 
-        disappear: 0, source: '/Hill_desert_003.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Hill_desert_003.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.003, 0.001],
         position: [-3, 0, -100]
     },
     {
         id: 4, 
-        disappear: 0, source: '/Hill_desert_004.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Hill_desert_004.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0007, 0.002, 0.001],
         position: [-3, 0, -100]
     },
     {
         id: 5, 
-        disappear: 0, source: '/Hill_desert_005.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Hill_desert_005.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0004, 0.002, 0.001],
         position: [0, 0, -100]
     },
     {
         id: 6, 
-        disappear: 0, source: '/Mountain_desert_001.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_001.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.001, 0.002, 0.001],
         position: [3, 0, -100]
     },
     {
         id: 7, 
-        disappear: 0, source: '/Mountain_desert_002.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_002.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0035, 0.0005, 0.001],
         position: [0, 0, -100]
     },
     {
         id: 8, 
-        disappear: 0, source: '/Mountain_desert_003.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_003.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.002, 0.001],
         position: [0, 0, -100]
     },
     {
         id: 9, 
-        disappear: 0, source: '/Mountain_desert_004.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_004.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.001, 0.002, 0.001],
         position: [3, 0, -100]
     },
     {
         id: 10, 
-        disappear: 0, source: '/Mountain_desert_005.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_005.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.001, 0.002, 0.001],
         position: [-3, 0, -100]
     },
     {
         id: 11, 
-        disappear: 0, source: '/Mountain_desert_006.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_006.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.001, 0.001],
         position: [0, 0, -100]
     },
     {
         id: 12, 
-        disappear: 0, source: '/Mountain_desert_007.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_007.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0015, 0.0005, 0.001],
         position: [0.3, 0, -100]
     },
     {
         id: 13, 
-        disappear: 0, source: '/Mountain_desert_008.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_008.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.002, 0.001],
         position: [3, 0, -100]
     },
     {
         id: 14, 
-        disappear: 0, source: '/Mountain_desert_009.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_009.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.001, 0.002, 0.001],
         position: [-3, 0, -100]
     },
     {
         id: 15, 
-        disappear: 0, source: '/Mountain_desert_010.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Mountain_desert_010.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.005, 0.003, 0.003],
         position: [0, 0, -100]
     },
     {
         id: 16, 
-        disappear: 0, source: '/Plateau_desert_001.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Plateau_desert_001.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.001, 0.001],
         position: [3, 0, -10]
     },
     {
         id: 17, 
-        disappear: 0, source: '/Plateau_desert_002.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Plateau_desert_002.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0005, 0.001, 0.001],
         position: [-3, 0, -100]
     },
     {
         id: 18, 
-        disappear: 0, source: '/Plateau_desert_003.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Plateau_desert_003.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0002, 0.004, 0.001],
         position: [0, 0, -10]
     },
     {
         id: 19,
-        disappear: 0, source: '/Plateau_desert_004.fbx',
-        texture: '/T_Mountains_desert.png',
+        disappear: 0, source: `${baseUrl}Plateau_desert_004.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0003, 0.0013, 0.001],
         position: [0, 0, -100]
@@ -162,8 +164,8 @@ export const fbxElements = [
     {
         id: 20,
         disappear: 0, 
-        source: '/Plateau_desert_005.fbx',
-        texture: '/T_Mountains_desert.png',
+        source: `${baseUrl}Plateau_desert_005.fbx`,
+        texture: `${baseUrl}T_Mountains_desert.png`,
         rotation: [0, 0, 0],
         scale: [0.0003, 0.001, 0.001],
         position: [0, 0, -100]
@@ -174,39 +176,39 @@ export const fbxElements = [
         rotation: [0, 0, 0],
         scale: [0.03, 0.03, 0.03],
         position: [0, 1.2, -100],
-        source: '/a_realistic_tiger__0118134023_texture.fbx',
-        texture: '/texture_0.png'
+        source: `${baseUrl}a_realistic_tiger__0118134023_texture.fbx`,
+        texture: `${baseUrl}texture_0.png`
     },
     {   
         id: 22,
         rotation: [2, 0, 4.3],
         scale: [0.002, 0.002, 0.002],
         position: [0, 0, -100],
-        source: '/model.fbx',
-        texture: '/tex_u1_v1.jpg'
+        source: `${baseUrl}model.fbx`,
+        texture: `${baseUrl}tex_u1_v1.jpg`
     },
     {
         id: 23,
         rotation: [0, 0, 0],
         scale: [0.4, 0.4, 0.4],
         position: [0, 0, -100],
-        source: '/cheeseburger.fbx',
-        texture: '/cheeseburger_diffuse.jpg'
+        source: `${baseUrl}cheeseburger.fbx`,
+        texture: `${baseUrl}cheeseburger_diffuse.jpg`  
     },
     {
         id: 24,
         rotation: [0, 0, 0],
         scale: [0.4, 0.4, 0.4],
         position: [0, 0, -100],
-        source: '/cheeseburger.fbx',
-        texture: '/cheeseburger_normal.jpg'
+        source: `${baseUrl}cheeseburger.fbx`,
+        texture: `${baseUrl}cheeseburger_normal.jpg`
     },
     {
         id: 25,
         rotation: [2, 0, 4.3],
         scale: [0.002, 0.002, 0.002],
         position: [0, 0, -10],
-        source: '/model.fbx',
-        texture: '/internal_ground_ao_texture.jpeg'
+        source: `${baseUrl}model.fbx`,
+        texture: `${baseUrl}internal_ground_ao_texture.jpeg`
     }
 ];
